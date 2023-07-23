3c4bd3a4685fd0c3639c13d7145dfeafad88d7f9
Jebep33 <87921319+Jebep33@users.noreply.github.com>
2023-07-23 22:28:14 +0800

    added js file

---
+++ myScript.js
@@ -0,0 +1,39 @@
+windows.onload = () => {
+	if(localStorage.getItem('content')) {
+		document.querySelector('.content').innerText = localStorage.getItem('content');
+  }
+}
+
+
+let editBtn = document.querySelector("#edit-welcome");
+let content = document.querySelector('#welcome');
+let saveBtn = document.querySelector("#save");
+var text = content.innerText;
+
+editBtn.addEventListener('click', () => {
+  // Toggle contentEditable on button click
+	content.contentEditable = true;
+  
+	content.style.border = "1px solid black";
+	saveBtn.style.display = "block";
+  
+  console.log(content.contentEditable);
+  console.log(content);
+  console.log(text);
+  
+});
+
+saveBtn.addEventListener('click', () => {
+	content.contentEditable = false;
+	content.style.border = "none";
+	saveBtn.style.display = "none";
+	
+	text = content.innerText;
+	
+	console.log(text);
+	
+	if(content.contentEditable === 'false') {
+  	localStorage.setItem('content',content.innerText);
+  }
+	
+});
